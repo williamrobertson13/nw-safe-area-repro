ba9b791f398d477a56fd6282d8f9d41323106302
William Robertson <williamrobertson@mystenlabs.com>
2024-11-16 19:48:41 -0500

    repro

--- components/ScreenContent.tsx
+++ components/ScreenContent.tsx
@@ -1,7 +1,5 @@
 import { Text, View } from 'react-native';
 
-import { EditScreenInfo } from './EditScreenInfo';
-
 type ScreenContentProps = {
   title: string;
   path: string;
@@ -12,14 +10,11 @@ export const ScreenContent = ({ title, path, children }: ScreenContentProps) => 
   return (
     <View className={styles.container}>
       <Text className={styles.title}>{title}</Text>
-      <View className={styles.separator} />
-      <EditScreenInfo path={path} />
       {children}
     </View>
   );
 };
 const styles = {
-  container: `items-center flex-1 justify-center`,
-  separator: `h-[1px] my-7 w-4/5 bg-gray-200`,
+  container: `p-safe mx-safe pt-safe-or-4 bg-red-400`,
   title: `text-xl font-bold`,
 };
